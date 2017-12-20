05b52c506133121f372eade703639b8a9efe9cf4
droidstick <droidstick@gmail.com>
2017-12-20 14:20:45 +0700

    feat: display books in their respective shelf

---
+++ src/Constants.js
@@ -0,0 +1,9 @@
+const SHELF_CURRENTLY_READING = 'currentlyReading';
+const SHELF_WANT_TO_READ = 'wantToRead';
+const SHELF_READ = 'read';
+
+export default{
+  SHELF_CURRENTLY_READING,
+  SHELF_WANT_TO_READ,
+  SHELF_READ
+}
